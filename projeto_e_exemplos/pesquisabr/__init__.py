46ff6d1ec706e1b880c63a790c4d01ca111a6c39
Luiz Anísio Vieira <luizanisio@gmail.com>
2020-12-22 12:20:32 -0300

    ajustes sem dependências do banco de dados

--- projeto_e_exemplos/pesquisabr/__init__.py
+++ projeto_e_exemplos/pesquisabr/__init__.py
@@ -1,5 +1,11 @@
-#name = "pesquisabr"
+name = "pesquisabr"
+### Pode ser necessário ajustes nesse arquivo
+### Como está ele funciona sem dependências com o banco de dados
+
 #import pesquisabr.pesquisabr
-#import pesquisabr.pesquisabr_memsql
+#import pesquisabr.pesquisabr
+#import pesquisabr.pesquisabr 
 #import pesquisabr.util
 #import pesquisabr.util_memsql
+from pesquisabr.pesquisabr import *
+#import pesquisabr.pesquisabr_memsql
