26e75373d615ca570e99eb19323b2dac4ea1f5e7
Luiz Anísio Vieira <luizanisio@gmail.com>
2020-12-23 19:13:44 -0300

    serviço exemplo para avaliação de regras

---
+++ projeto_e_exemplos/servico_regras/app_regras.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+# teste para aplicação de regras
+# http://localhost:8000/analisar_regras?texto=esse%20texto%20tem%20uma%20receita%20de%20pao%20e%20de%20bolo%20legal&criterio=texto&debug=0
+# teste para análise de critérios
+# http://localhost:8000/analisar_criterio?texto=esse%20texto%20legal&criterio=texto&debug=1
+
+# debug=qualquer coisa vai ativar o retorno com mais informações e print de debug
+
+import time
+from flask import Flask, jsonify, request
+from multiprocessing import Process, Value, Queue
+import os
+import json
+from datetime import datetime, timedelta
+
+import sys
+sys.path.insert(1, '../pesquisabr')
+from pesquisabr import PesquisaBR, RegrasPesquisaBR
+
+app = Flask(__name__)
+
+ARQ_CONFIG = './config.json'
+ARQ_REGRAS = './regras.json'   # arquivo texto no formato [{regra}, {regra}, {regra}]
+CONFIG = {}
+REGRAS = []
+dthr_regras = None
+
+def carregar_config():
+    global CONFIG
+    _config = {}
+    if os.path.isfile(ARQ_CONFIG):
+       with open(ARQ_CONFIG,mode='r') as f:
+           _config = f.read().replace('\n',' ').strip()
+           if _config and _config[0]=='{' and _config[-1]=='}':
+              _config = json.loads(_config)
+    # correções do tempo de refresh das regras
+    _config['tempo_regras'] = _config.get('tempo_regras',300)
+    if (type(_config['tempo_regras']) != int) or (_config['tempo_regras']<5):
+       _config['tempo_regras'] = 5
+    # atualização da configuração
+    CONFIG = _config
+
+def carregar_regras():
+    global REGRAS, dthr_regras
+    # recarrega as regras a cada 5 minutos
+    # é um exemplo pois poderia recarregar do banco de dados ou de algum microserviço
+    if dthr_regras and (datetime.now()-dthr_regras).total_seconds()<300:
+        print(f'Regras em cache: {len(REGRAS)}', dthr_regras)
+        return 
+    dthr_regras = datetime.now()
+    _regras = []
+    if os.path.isfile(ARQ_REGRAS):
+        with open(ARQ_REGRAS,mode='r') as f:
+            _regras = f.read().strip()
+            _regras = json.loads(_regras)
+            _regras = _regras.get('regras')
+            _regras = sorted(_regras, key=lambda k:k.get('grupo',''))    
+        REGRAS = _regras 
+    else:
+        REGRAS = []
+    print(f'Número de regras carregadas: {len(REGRAS)}')
+
+@app.route('/health', methods=['GET'])
+def get_health():
+    _criterios = 'casas ADJ2 papeis PROX10 legal PROX10 seriado'
+    _texto = 'a casa de papel é um seriado muito legal'
+    pb = PesquisaBR(texto=_texto, criterios=_criterios, print_debug=False)
+    if not pb.retorno():
+       print('\n\n')
+       print('=========================================================')
+       print('=    ERRO NO CRITÉRIO DE PESQUISA DO HEALTH CHECK       =')
+       pb.print_resumo() 
+       print('=========================================================')
+       print('\n\n')
+       raise Exception(f'Health check - ERRO na avaliação do critério de pesquisa')
+    print('\n===== HEALTH CHECK OK =====\n')
+    return jsonify({'ok': True, 'criterios': pb.criterios, 'criterios_aon': pb.criterios_and_or_not, 'texto': pb.texto })
+
+
+# recebe {'texto': 'texto para ser analisado pelas regras'}
+# retorna {'retorno': true/false }
+@app.route('/analisar_regras', methods=['GET','POST'])
+def analisar_regras():
+    carregar_regras()
+    global REGRAS
+    _texto = str(request.values.get("texto",""))
+    _debug = str(request.values.get("debug",""))
+    if _debug:
+        print(type(_texto),_texto)
+    _criterios = 'teste PROX10 legal'
+    pbr = RegrasPesquisaBR(regras=[], print_debug=False)
+    pbr.regras = REGRAS # não reprocessa a ordenação, só para diminuir o processamento
+    res = pbr.aplicar_regras(texto=_texto)
+    if _debug:
+        return jsonify({'retorno': res, 'regras': pbr.regras, 'texto': _texto })
+    return jsonify({'retorno': res})
+
+# recebe {'texto': 'texto para ser analisado pelas regras', 'criterio':'critérios de pesquisa'}
+# retorna {'retorno': true/false }
+@app.route('/analisar_criterio', methods=['GET','POST'])
+def analisar_criterio():
+    _texto = str(request.values.get("texto",""))
+    _criterios = str(request.values.get("criterio",""))
+    _debug = str(request.values.get("debug",""))
+    if not _criterios:
+       _criterios = str(request.values.get("criterios",""))
+    if _debug:
+        print('Texto: ', type(_texto),_texto)
+        print('Critério: ', type(_criterios),_criterios)
+    pb = PesquisaBR(texto=_texto, criterios=_criterios, print_debug=False)
+    if _debug:
+       return jsonify({'retorno': pb.retorno(), 'criterios': pb.criterios, 'criterios_aon': pb.criterios_and_or_not, 'texto': pb.texto })
+    return jsonify({'retorno': pb.retorno()})
+
+
+if __name__ == "__main__":
+    # carrega as configurações
+    carregar_config()
+    # carrega as regras
+    carregar_regras()
+
+    print('#########################################')
+    print('Iniciando o serviço')
+    print('Configuações: ', json.dumps(CONFIG))
+    print('-----------------------------------------')
+    app.run(host='0.0.0.0', debug=True,port=8000) 
+
