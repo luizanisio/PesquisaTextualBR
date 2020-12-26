c850798e2d4714dab690854435d4eb7f020f0e10
Luiz Anísio Vieira <luizanisio@gmail.com>
2020-12-26 14:39:39 -0300

    regras com regex, filtros, tags, cabeçalhos e rodapés

--- projeto_e_exemplos/servico_regras/app_regras.py
+++ projeto_e_exemplos/servico_regras/app_regras.py
@@ -12,7 +12,9 @@ from multiprocessing import Process, Value, Queue
 import os
 import json
 from datetime import datetime, timedelta
+import re 
 
+# configuração de caminho para o componente
 import sys
 sys.path.insert(1, '../pesquisabr')
 from pesquisabr import PesquisaBR, RegrasPesquisaBR
@@ -24,6 +26,12 @@ ARQ_REGRAS = './regras.json'   # arquivo texto no formato [{regra}, {regra}, {re
 CONFIG = {}
 REGRAS = []
 dthr_regras = None
+
+# configuração do cache de regras - 5 minutos 
+from cachetools import cached, TTLCache
+from threading import RLock
+cache_filtros_regras = TTLCache(maxsize=2000, ttl=5 * 60) 
+lock = RLock()
 
 def carregar_config():
     global CONFIG
@@ -40,6 +48,7 @@ def carregar_config():
     # atualização da configuração
     CONFIG = _config
 
+REGEX_CORRIGE_TAGS = re.compile(r'[,;#\s]+')
 def carregar_regras():
     global REGRAS, dthr_regras
     # recarrega as regras a cada 5 minutos
@@ -50,14 +59,60 @@ def carregar_regras():
     _regras = []
     if os.path.isfile(ARQ_REGRAS):
         with open(ARQ_REGRAS,mode='r') as f:
-            _regras = f.read().strip()
-            _regras = json.loads(_regras)
-            _regras = _regras.get('regras')
-            _regras = sorted(_regras, key=lambda k:k.get('grupo',''))    
-        REGRAS = _regras 
+            _regras = f.read()
+        _regras = json.loads(_regras.strip())
+        _regras = _regras.get('regras')
+        _regras = sorted(_regras, key=lambda k:k.get('grupo',''))    
+        # corrige as tags das regras usando espaço como separador (para o regex achar um dos termos do filtro)
+        _regras_tags = []
+        for r in _regras:
+            r['tags'] = ' '+REGEX_CORRIGE_TAGS.sub(' ', r.get('tags','')) + ' '
+            r['qtd_cabecalho'] = r.get('qtd_cabecalho',0)
+            r['qtd_rodape'] = r.get('qtd_rodape',0)
+            # se a regra for um regex, valida ele 
+            if regex_valido(r.get('regra'), r.get('rotulo')) and regex_valido('r:'+r.get('extracao',''), r.get('rotulo')):
+               _regras_tags.append(r)
+        REGRAS = _regras_tags
     else:
         REGRAS = []
     print(f'Número de regras carregadas: {len(REGRAS)}')
+
+# retorna true para regex válido em regras ou extrações
+def regex_valido(txt_regex, rotulo):
+    if (not txt_regex) or (txt_regex[:2] not in {'r:','R:'}):
+        return True
+    try:
+        re.compile(str(txt_regex))
+        print(f'REGEX OK: rótulo "{rotulo}" com o texto de regex: {txt_regex}')
+        return True
+    except re.error:
+        pass 
+    print(f'REGEX inválido: rótulo "{rotulo}" com o texto de regex: {txt_regex}')
+    return False
+
+# retorna apenas as regras de um grupo específico e/ou que contém alguma das tags informadas
+# tags_lista é uma lista de tags do tipo "tag1 tag2 tag3"
+# com isso, se tiver uma das tags já é aceito no filtro
+# o cache evita refazer diversos filtros repetidamente
+# cabecalho e rodapé serve para recortar o texto e analisar apenas o início, fim ou início e fim dele
+@cached(cache_filtros_regras, lock=lock)
+def regras_filtradas(tags_lista = None, grupo = None):
+    carregar_regras()
+    global REGRAS
+    if (not grupo) and (not tags_lista):
+        #print(f'>>> Regras não filtradas: {len(REGRAS)}')
+        return REGRAS
+    tags_lista = '' if not tags_lista else '( ' + REGEX_CORRIGE_TAGS.sub(' ', str(tags_lista)).strip().replace(' ',' )|( ') + ' )'
+    re_tags_lista = re.compile(tags_lista)
+    res = []
+    for r in REGRAS:
+        if grupo and str(grupo) != str(r.get('grupo','')):
+            continue
+        if tags_lista and not re_tags_lista.search(r.get('tags','')):
+            continue
+        res.append(r)
+    #print(f'>>> Regras filtradas: {len(res)}/{len(REGRAS)} >> [{tags_lista}]')
+    return res
 
 def get_post(req:request):
     res = (
@@ -95,11 +150,14 @@ def analisar_regras():
     dados = get_post(request)
     _texto = str(dados.get("texto",""))
     _detalhar = str(dados.get("detalhar","")) not in ("","0","False")
-    _criterios = 'teste PROX10 legal'
+    _tags = str(dados.get("tags",""))
+    _grupo = str(dados.get("grupo",""))
     pbr = RegrasPesquisaBR(regras=[], print_debug=False)
-    pbr.regras = REGRAS # não reprocessa a ordenação, só para diminuir o processamento
+    # não envia como parâmetro para o construtor para não reprocessar a ordenação, só para diminuir o processamento
+    # filtra as regras com uma das tags e/ou com o grupo informado
+    pbr.regras = regras_filtradas(tags_lista=_tags, grupo=_grupo) 
     res = pbr.aplicar_regras(texto=_texto, detalhar=_detalhar)
-    return res
+    return jsonify(res)
 
 # recebe {'texto': 'texto para ser analisado pelas regras', 'criterio':'critérios de pesquisa'}
 # retorna {'retorno': true/false }
