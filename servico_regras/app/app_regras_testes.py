00f7e211f98678f1e84a672e9ce68d66f7352afc
Luiz Anísio Vieira <luizanisio@gmail.com>
2023-05-09 14:54:15 -0300

    pesquisabr - componente 1.0.3.3

--- servico_regras/app/app_regras_testes.py
+++ servico_regras/app/app_regras_testes.py
@@ -6,21 +6,23 @@ import unittest
 from pesquisabr import PesquisaBR
 from pesquisabr import TESTES_RETORNO_COMPLETO, TESTES_EXTRACAO, TESTES_EXTRACAO_REGRAS, \
                                                  TESTES_COMPLETOS, TESTES_TEXTOS, TESTES_CRITERIOS, \
-                                                 TESTES_GRIFAR, TESTES_CABECALHO_RODAPE, TESTE_COM_REMOVER                       
+                                                 TESTES_GRIFAR, TESTES_CABECALHO_RODAPE, TESTE_COM_REMOVER, \
+                                                 TESTES_BASICOS_RE_PRONTOS
 from app_config import PATH_URL_API
 # cria um smart_request mais resiliente a falhas pois o teste pode sobrecarregar o serviço
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
-
+import json
+import time
 
 def smart_request():
     # https://findwork.dev/blog/advanced-usage-python-requests-timeouts-retries-hooks/#retry-on-failure
     retry_strategy = Retry(
         total=5,
         status_forcelist=[429, 500, 502, 503, 504],
-        #allowed_methods=["HEAD", "GET", "POST", "OPTIONS"],
-        method_whitelist=["HEAD", "GET", "POST", "OPTIONS"],
+        allowed_methods=["HEAD", "GET", "POST", "OPTIONS"],
+        #method_whitelist=["HEAD", "GET", "POST", "OPTIONS"],
         backoff_factor=0.5
     )
     adapter = HTTPAdapter(max_retries=retry_strategy)
@@ -29,12 +31,13 @@ def smart_request():
     http.mount("http://", adapter)
     return http
 
+TIMEOUT = 60 * 10
 def smart_request_get_post(url, **kwargs):
     session = smart_request()
     if any(kwargs):
-       r = session.post(url, **kwargs)
+       r = session.post(url, timeout = TIMEOUT, **kwargs)
     else:
-       r = session.get(url)
+       r = session.get(url, timeout = TIMEOUT)
     if r.status_code == 200:
         r = r.json()
     else:
@@ -83,6 +86,45 @@ class TestAppRegrasCriterios(TestAppRegrasBase):
         super().__init__(methodName)
         self.maxDiff = 5000
 
+    def teste_regex_timed_out(self):
+        for chave, esperado in [('teste-timeout',408),('testetimeout',408),('detalhar', 0)]:
+            with self.subTest(f'Regex timed out'):
+                dados = {"texto": '123', f'{chave}': True, 'outro_dado' : 1}
+                try:
+                    res_code = 0
+                    smart_request_get_post(f'{PATH_URL_API}analisar_regras',json = dados)
+                except requests.HTTPError as e:
+                    res_code = e.response.status_code
+                    dados = json.loads(e.response.content)
+                    # verifica o dicionário de retorno quando ocorre erro de timeout
+                    self.assertEqual(1, dados.get('outro_dado',0))
+                    self.assertEqual(408, dados.get('status',0))
+                    self.assertEqual('regex timed out', dados.get('erro',''))
+                self.assertEqual( res_code, esperado)
+
+    def teste_catastrophic_backtracking(self):
+        with self.subTest(f'Catastrophic backtracking'):
+            texto, esperado = '012345678901234567890123456789z',False
+            regra = 'r:^(\d+)*$'
+            dados = {"texto": texto, 
+                    "criterios" : regra,
+                    "detalhar" : 1}
+            r = smart_request_get_post(f'{PATH_URL_API}analisar_criterio',json = dados)
+            retorno = r.get('retorno')
+            self.assertEqual(retorno, esperado)
+
+    def teste_texto_simples(self):
+        for i, teste in enumerate(TEXTOS_TESTES_SIMPLES_QUEBRAS):
+            with self.subTest(f'Textos testes simples {i+1}'):
+                texto, esperado = teste
+                dados = {"texto": texto, 
+                        "criterios" : "x",
+                        "detalhar" : 1}
+                r = smart_request_get_post(f'{PATH_URL_API}analisar_criterio',json = dados)
+                retorno = PesquisaBR.corrige_quebras_para_br(r.get('texto',''))
+                esperado = PesquisaBR.corrige_quebras_para_br(esperado)
+                self.assertDictEqual({'r':retorno}, {'r':esperado})
+
     def teste_criterios_regex(self):
         dados = {"texto": "esse ofício 12", 
                  "criterios" : "r:esse\\W.*12$",
@@ -116,6 +158,21 @@ class TestAppRegrasCriterios(TestAppRegrasBase):
                      "retorno": True,
                      "texto": "esse e um texto legal"}
         self.assertDictEqual(r, esperado)
+
+    def teste_criterio_re_pronto(self):
+        testes = TESTES_SIMPLES_RE_PRONTOS + TESTES_BASICOS_RE_PRONTOS
+        for i, teste in enumerate(testes):
+            texto = teste.get('texto','')
+            criterio = teste.get('criterio','')
+            criterio = f'r:{criterio}' if criterio[:2] != 'r:' else criterio
+            with self.subTest(f'Regex Prontos {i+1}'):
+                 dados = {"texto": texto,
+                          "detalhar": 1, "extrair" : 0, "grifar" : 0, 
+                          "criterios" : criterio}
+            r = smart_request_get_post(f'{PATH_URL_API}analisar_criterio',json = dados)
+            if r.get('retorno') != teste.get('retorno'):
+               print('Retorno recebido: ', r)
+            self.assertEqual(r.get('retorno'), teste.get('retorno') )
 
     def teste_criterio_dict(self):
         dados = {"texto": {"a": "a casa de papel é um seriado bem interessante número123", "b": "segundo"}, 
@@ -247,14 +304,38 @@ class TestAppRegrasCriterios(TestAppRegrasBase):
         self.assertDictEqual(r, esperado)
 
     def teste_limpeza_cache(self):
+        # controle do id de cache e limpeza
+        r = smart_request_get_post(f'{PATH_URL_API}health')
+        id_cache = r['cache']['id_cache']
+        dt_cache = r['cache']['data_carga_regras']
+        self.assertTrue(r.get('ok'))
         r = smart_request_get_post(f'{PATH_URL_API}cache')
         self.assertTrue(r.get('ok'))
-        r = smart_request_get_post(f'{PATH_URL_API}health')
-        self.assertTrue(r.get('ok'))
-        dados = {"texto": "teste limpar cache", 
-                 "detalhar":0, "extrair" : 0, 'rodando-testes' : 1, "tags":"teste", "modo_teste": 1}
+
+        # testando a limpeza de cache - modo teste
+        dados = {"texto": "teste limpar cache", "detalhar":0, "extrair" : 0, 'rodando-testes' : 1, "tags":"teste", "modo_teste": 1}
         r = smart_request_get_post(f'{PATH_URL_API}analisar_regras',json = dados)
         self.assertTrue(r.get('cache_limpo'))
+        dados['modo_teste'] = True
+        r = smart_request_get_post(f'{PATH_URL_API}analisar_regras',json = dados)
+        self.assertTrue(r.get('cache_limpo'))
+
+        # testando a limpeza de cache - limpar cache
+        dados = {"texto": "teste limpar cache", "detalhar":0, "extrair" : 0, 'rodando-testes' : 1, "tags":"teste", "limpar_cache": True}
+        r = smart_request_get_post(f'{PATH_URL_API}analisar_regras',json = dados)
+        self.assertTrue(r.get('cache_limpo'))
+
+        # testando se a carga no banco não foi refeita
+        # vai dar erro caso ocorra um recarregamento ou atualização das regras nesse momento
+        # vai dar erro se as datas são de instâncias diferentes. O teste funciona se rodado em uma instância
+        #r = smart_request_get_post(f'{PATH_URL_API}health')
+        #id_cache2 = r['cache']['id_cache']
+        #dt_cache2 = r['cache']['data_carga_regras']
+        #self.assertTrue(r.get('ok'))
+        #self.assertTrue(id_cache == id_cache2)
+        #print('Caches datas: ', dt_cache, dt_cache2)
+        #self.assertTrue(dt_cache == dt_cache2)
+
 
     def testes_retorno_completo(self):
         for i, teste in enumerate(TESTES_RETORNO_COMPLETO):
@@ -336,6 +417,52 @@ class TestAppRegrasCriterios(TestAppRegrasBase):
             esperado.pop('texto')
             self.assertDictEqual(r, esperado)
 
+    def testes_filtros_regras_tags(self):
+        # rodando-testes é para controlar as conversões de saída
+        # caso tenha uma chave na regra "tags-....", a regra só é aplicada de coincidir com pelo menos uma das tags enviadas em tags-....
+        # pode ser tags- ou tags_
+        dados = {"texto": "teste um dois tres quatro cinco seis a b c ", "detalhar": 1, 
+                 "primeiro_do_grupo" : False, 'rodando-testes' : 1,
+                 'regras_externas' : REGRAS_TESTES_TAGS}
+        testes = [{"esperado" : ['neutro1','neutro2'] },
+                  {"tags_numero" : "um dois tres", "esperado" : ['teste1', 'teste2','teste3','neutro1','neutro2'] },
+                  {"tags_numero" : "1 2 3", "esperado" : ['teste1', 'teste2','teste3','neutro1','neutro2'] },
+                  {"tags_numero" : "um 1 2 dois 3 tre quatro cinco", "esperado" : ['teste1', 'teste2','teste3','neutro1','neutro2'] },
+                  {"tags_numero" : "um dois", "esperado" : ['teste1', 'teste2','neutro1','neutro2'] },
+                  {"tags_numero" : "um tres", "esperado" : ['teste1', 'teste3','neutro1','neutro2'] },
+                  {"tags_numero" : "dois", "esperado" : ['teste2','neutro2','neutro1'] },
+                  {"tags_numero" : "*", "esperado" : ['teste1', 'teste2','teste3','neutro1','neutro2'] },
+                  {"tags_numero" : "*", "tags":"neutro", "esperado" : ['neutro1','neutro2'] },
+                  {"tags_letra" : "*", "esperado" : ['testea', 'testeb','testec','neutro1','neutro2'] },
+                  {"tags_letra" : "a", "esperado" : ['testea', 'neutro1','neutro2'] },
+                  {"tags_letra" : "c a", "esperado" : ['testea', 'testec', 'neutro1','neutro2'] },
+                  {"tags_letra" : "c a", "tags_numero" : "1 dois", "esperado" : ['testea', 'testec', 'teste1', 'teste2', 'neutro1','neutro2'] },
+                  {"tags_letra" : "*", "tags_numero" : "*", "esperado" : ['testea', 'testeb', 'testec', 'teste1', 'teste2', 'teste3', 'neutro1','neutro2'] },
+                  ]
+        for teste in testes:
+            with self.subTest(f'Filtro de tags: {teste.get("tags_numero","-")}'):
+                if teste.get("tags_numero"):
+                   dados["tags_numero"] = teste["tags_numero"]
+                else:
+                   dados.pop("tags_numero",None)
+                if teste.get("tags_letra"):
+                   dados["tags_letra"] = teste["tags_letra"]
+                else:
+                   dados.pop("tags_letra",None)
+                if teste.get("tags"):
+                   dados["tags"] = teste["tags"]
+                else:
+                   dados.pop("tags",None)
+                esperado = teste['esperado']
+                r = smart_request_get_post(f'{PATH_URL_API}analisar_regras',json = dados)
+                r = self.limpa_request(r)
+                #print('Dados: ', dados)
+                #print('Retorno: ', r)
+                retornado = r.get('rotulos',[])
+                esperado.sort()
+                retornado.sort()
+                self.assertEqual(esperado, retornado)
+
     def testes_dados_retorno(self):
         with self.subTest('Retorno PesquisaBR'):
             dados = {"texto": "esse teste é simples 123,45 123.123 simples", 
@@ -411,8 +538,8 @@ class TestAppRegrasCriterios(TestAppRegrasBase):
                  dados = {"texto": teste['texto'], "detalhar": 1, "extrair" : 0, 
                           'rodando-testes' : 1, 'criterio' : teste['criterio']}
             r = smart_request_get_post(f'{PATH_URL_API}analisar_criterio',json = dados)
-            esperado = teste.get('texto_limpo','').replace('\n','<br>')
-            retornado = r.get('texto', '').replace('\n','<br>')
+            esperado = PesquisaBR.corrige_quebras_para_br(teste.get('texto_limpo',''),True)
+            retornado = PesquisaBR.corrige_quebras_para_br(r.get('texto', ''),True)
             #print('Esperado: ', esperado)
             if esperado != retornado:
                print('Esperado: ', f'|{esperado}|')
@@ -426,13 +553,48 @@ REGRAS_TESTES = [
     {"grupo" : "grupo_oficio", "rotulo": "oficio", "regra": "r:oficio\\s?(n.{0,10})?\\d+", "tags": "teste oficio", "qtd_cabecalho":20, "qtd_rodape":20, "filtro_tipo" : "grupo"},
     {"grupo" : "grupo_multi", "rotulo": "multi2", "regra": "R:multi", "tags": "multi", "qtd_cabecalho":0, "qtd_rodape":0, "filtro_tipo" : "multi", "ordem": 1},
     {"grupo" : "grupo_multi", "rotulo": "multi1", "regra": "multi", "tags": "multi", "qtd_cabecalho":0, "qtd_rodape":0, "filtro_tipo" : "multi", "ordem": 0},
-    {"grupo" : "grupo_multi", "rotulo": "multi3", "regra": "multi", "tags": "multi", "qtd_cabecalho":0, "qtd_rodape":0, "filtro_tipo" : "multi", "ordem": 3}
+    {"grupo" : "grupo_multi", "rotulo": "multi3", "regra": "multi", "tags": "multi", "qtd_cabecalho":0, "qtd_rodape":0, "filtro_tipo" : "multi", "ordem": 3},
+    {"grupo" : "tags tipo", "rotulo": "tagtipo1", "regra": "tagtipo1", "tags-tipo": "um dois tres", "qtd_cabecalho":0, "qtd_rodape":0, "ordem": 1},
  ]
+
+REGRAS_TESTES_TAGS = [
+    {"grupo" : "grupo_um", "rotulo": "teste1", "regra": "r:um", "tags": "geral", "tags_numero": "um 1"},
+    {"grupo" : "grupo_dois", "rotulo": "teste2", "regra": "r:dois", "tags": "geral", "tags_numero": "dois 2"},
+    {"grupo" : "grupo_tres", "rotulo": "teste3", "regra": "r:tres", "tags": "geral","tags_numero": "tres 3"},
+    {"grupo" : "grupo_neutro1", "rotulo": "neutro1", "regra": "r:teste", "tags" : "neutro n"},
+    {"grupo" : "grupo_neutro2", "rotulo": "neutro2", "regra": "r:teste", "tags" : "neutro n"},
+    {"grupo" : "grupo_a", "rotulo": "testea", "regra": r"r:\ba\b", "tags": "geral", "tags_letra": "a"},
+    {"grupo" : "grupo_b", "rotulo": "testeb", "regra": r"r:\bb\b", "tags": "geral", "tags_letra": "b"},
+    {"grupo" : "grupo_c", "rotulo": "testec", "regra": r"r:\bc\b", "tags": "geral","tags_letra": "c"},
+ ]
+
+TEXTOS_TESTES_SIMPLES_QUEBRAS = [
+    ('teste de texto simples', 'teste de texto simples'),
+    ('à á À Á teste ó Ò Ó ò çcÇC', 'a a a a teste o o o o cccc'),
+    ('teste<br>testa\\nnada', 'teste \n testa \n nada'),
+    (r'teste<br>testa\\nnada', 'teste \n testa nnada'),
+    (['à á À Á teste ó Ò Ó ò çcÇC', 'teste<br>testa\\nnada'], ['a a a a teste o o o o cccc', 'teste \n testa \n nada']),
+    ({'a': 'à á À Á teste ó Ò Ó ò çcÇC', 'b':'teste<br>testa\\nnada'}, {'A':'a a a a teste o o o o cccc', 'B':'teste \n testa \n nada'}),
+]
+
+TESTES_SIMPLES_RE_PRONTOS = [
+    {'texto' : 'esse é um teste de regex pronto por termos', 'criterio': 'r:test<FIMTERMO><TERMO>{0,3}por', 'retorno':True},
+    {'texto' : 'esse é um teste de regex pronto por termos', 'criterio': 'r:test<FIMTERMO><TERMO>{0,2}por', 'retorno':False},
+    {'texto' : 'esse é um teste de cpf: 123.456.789-10 ', 'criterio': 'r:<CPF:12345678910>', 'retorno':True},
+    {'texto' : 'esse é um teste de cpf: 123456789-10 ', 'criterio': 'r:<CPF:12345678910>', 'retorno':True},
+    {'texto' : 'esse é um teste de cpf: 12345678910 ', 'criterio': 'r:<CPF:12345678910>', 'retorno':True},
+    {'texto' : 'esse é um teste de cpf: 123.456.789-11 ', 'criterio': 'r:<CPF:12345678910>', 'retorno':False},
+    {'texto' : 'esse é um teste de oab DF 1234 a', 'criterio': 'r:<OAB:df1234a>', 'retorno':True},
+    {'texto' : 'esse é um teste de oab DF-1234/a', 'criterio': 'r:<OAB:df1234a>', 'retorno':True},
+    {'texto' : 'esse é um teste de oab DF 1234, a', 'criterio': 'r:<OAB:df1234a>', 'retorno':False},
+]
 
 if __name__ == '__main__':
     print('##########################################################################')
     print('Iniciando os testes na URL: ', PATH_URL_API)
     print('==========================================================================')
     TestAppRegrasCriterios.TESTE_RESUMIDO = True
-    unittest.main(buffer=True, failfast=True)
-
+    unittest.main(buffer=False, failfast=True)
+    
+    # linha para rodar um teste específico
+    # python app_regras_testes.py TestAppRegrasCriterios.testes_filtros_regras_tags
